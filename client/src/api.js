6de0649093bfd8c4a7a104e1765b72c55ba655be
shashank kumar <shashankmishra00026@gmail.com>
2025-08-10 01:21:55 +0530

    change in url

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:4040', // Explicitly set baseURL
   withCredentials: true,
 });
 
