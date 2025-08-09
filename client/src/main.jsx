576871afa78ab109f64b394f93fa666ec0624aef
shashank kumar <shashankmishra00026@gmail.com>
2025-08-10 01:28:07 +0530

    changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { ThemeProvider } from "./ThemeContext";
+import axios from 'axios'; // New import
+
+// Set axios default baseURL globally
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:4040';
+axios.defaults.withCredentials = true; // Also set withCredentials globally
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
