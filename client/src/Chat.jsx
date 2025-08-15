a6d99cb84d6df93f68e33e4dfaa8507646177c37
shashank kumar <shashankmishra00026@gmail.com>
2025-08-15 16:27:20 +0530

    feat: use dynamic websocket url

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -24,7 +24,7 @@ export default function Chat() {
   }, [selectedUserId]);
 
   function connectToWs() {
-    const ws = new WebSocket('ws://localhost:4040');
+    const ws = new WebSocket(import.meta.env.VITE_WS_URL);
     setWs(ws);
     ws.addEventListener('message', handleMessage);
     ws.addEventListener('close', () => {
