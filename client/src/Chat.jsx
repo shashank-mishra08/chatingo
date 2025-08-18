c02545bc650a51647d83e4bc7276458d18d86bba
shashank kumar <shashankmishra00026@gmail.com>
2025-08-18 19:37:00 +0530

    fixed the issue of place holder and white space appearing

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -179,51 +179,51 @@ export default function Chat() {
             className={"text-sm py-1 px-2 border rounded-sm " + (theme === 'light' ? 'bg-blue-100 text-gray-500' : 'bg-gray-600 text-gray-200')}>logout</button>
         </div>
       </div>
-      <div className={"flex flex-col chat-main p-2 " + (theme === 'light' ? 'bg-blue-50' : 'bg-gray-600') + (selectedUserId ? ' flex' : ' hidden sm:flex')}>
-        <div className="flex-grow">
+      <div className={"flex flex-col chat-main " + (theme === 'light' ? 'bg-blue-50' : 'bg-gray-600') + (selectedUserId ? ' flex' : ' hidden sm:flex')}>
+        <div className="flex-grow p-2 flex flex-col">
           {!selectedUserId && (
             <div className="flex h-full flex-grow items-center justify-center">
               <div className={(theme === 'light' ? 'text-gray-300' : 'text-gray-400')}>&larr; Select a person from the sidebar</div>
             </div>
           )}
           {!!selectedUserId && (
-            <div className="relative h-full">
-               <div className="flex items-center mb-2">
-                  <button onClick={() => setSelectedUserId(null)} className="sm:hidden text-gray-500 mr-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
-                    </svg>
-                  </button>
-                  <div className="flex items-center">
-                     <Avatar online={onlinePeople.hasOwnProperty(selectedUserId)} username={onlinePeople[selectedUserId] || offlinePeople[selectedUserId]?.username} userId={selectedUserId} />
-                     <span className="ml-2 font-bold">{onlinePeople[selectedUserId] || offlinePeople[selectedUserId]?.username}</span>
-                  </div>
+            <>
+              <div className="flex items-center mb-2 border-b pb-2 border-gray-200">
+                <button onClick={() => setSelectedUserId(null)} className="sm:hidden text-gray-500 mr-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" />
+                  </svg>
+                </button>
+                <div className="flex items-center">
+                   <Avatar online={onlinePeople.hasOwnProperty(selectedUserId)} username={onlinePeople[selectedUserId] || offlinePeople[selectedUserId]?.username} userId={selectedUserId} />
+                   <span className="ml-2 font-bold">{onlinePeople[selectedUserId] || offlinePeople[selectedUserId]?.username}</span>
                 </div>
-              <div className="overflow-y-scroll absolute top-10 left-0 right-0 bottom-2">
-                {messagesWithoutDupes.map(message => (
-                  <div key={message._id} className={(message.sender === id ? 'text-right': 'text-left')}>
-                    <div className={"text-left inline-block p-2 my-2 rounded-md text-sm " +(message.sender === id ? 'bg-blue-500 text-white' : (theme === 'light' ? 'bg-white text-gray-500' : 'bg-gray-800 text-white'))}>
-                      {message.text}
-                      {message.file && (
-                        <div className="">
-                          <a target="_blank" className="flex items-center gap-1 border-b" href={axios.defaults.baseURL + '/uploads/' + message.file}>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
-                              <path fillRule="evenodd" d="M18.97 3.659a2.25 2.25 0 00-3.182 0l-10.94 10.94a3.75 3.75 0 105.304 5.303l7.693-7.693a.75.75 0 011.06 1.06l-7.693 7.693a5.25 5.25 0 11-7.424-7.424l10.939-10.94a3.75 3.75 0 115.303 5.304L9.097 18.835l-.008.008-.007.007-.002.002-.003.002A2.25 2.25 0 015.91 15.66l7.81-7.81a.75.75 0 011.061 1.06l-7.81 7.81a.75.75 0 001.054 1.068L18.97 6.84a2.25 2.25 0 000-3.182z" clipRule="evenodd" />
-                            </svg>
-                            {message.file}
-                          </a>
-                        </div>
-                      )}
-                    </div>
-                  </div>
-                ))}
-                <div ref={divUnderMessages}></div>
               </div>
-            </div>
+              <div className="flex-grow overflow-y-auto">
+                  {messagesWithoutDupes.map(message => (
+                    <div key={message._id} className={(message.sender === id ? 'text-right': 'text-left')}>
+                      <div className={"text-left inline-block p-2 my-2 rounded-md text-sm " +(message.sender === id ? 'bg-blue-500 text-white' : (theme === 'light' ? 'bg-white text-gray-500' : 'bg-gray-800 text-white'))}>
+                        {message.text}
+                        {message.file && (
+                          <div className="">
+                            <a target="_blank" className="flex items-center gap-1 border-b" href={axios.defaults.baseURL + '/uploads/' + message.file}>
+                              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
+                                <path fillRule="evenodd" d="M18.97 3.659a2.25 2.25 0 00-3.182 0l-10.94 10.94a3.75 3.75 0 105.304 5.303l7.693-7.693a.75.75 0 011.06 1.06l-7.693 7.693a5.25 5.25 0 11-7.424-7.424l10.939-10.94a3.75 3.75 0 115.303 5.304L9.097 18.835l-.008.008-.007.007-.002.002-.003.002A2.25 2.25 0 015.91 15.66l7.81-7.81a.75.75 0 011.061 1.06l-7.81 7.81a.75.75 0 001.054 1.068L18.97 6.84a2.25 2.25 0 000-3.182z" clipRule="evenodd" />
+                              </svg>
+                              {message.file}
+                            </a>
+                          </div>
+                        )}
+                      </div>
+                    </div>
+                  ))}
+                  <div ref={divUnderMessages}></div>
+              </div>
+            </>
           )}
         </div>
         {!!selectedUserId && (
-          <form className="flex gap-2" onSubmit={sendMessage}>
+          <form className="flex gap-2 p-2" onSubmit={sendMessage}>
             <input type="text"
                    value={newMessageText}
                    onChange={ev => setNewMessageText(ev.target.value)}
