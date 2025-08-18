802b4a5392f9f4800c04ffe4b5b574ec873b3b0a
shashank kumar <shashankmishra00026@gmail.com>
2025-08-18 18:54:53 +0530

    changed width in form again

--- client/src/RegisterAndLoginForm.jsx
+++ client/src/RegisterAndLoginForm.jsx
@@ -92,7 +92,7 @@ export default function RegisterAndLoginForm() {
         </h1>
         {/* this form for registering user */}
         <Card>
-          <form className="w-75 mx-auto mb-12" onSubmit={handleSubmit}>
+          <form className="w-85 mx-auto mb-12" onSubmit={handleSubmit}>
 
 
             <input value={username}
